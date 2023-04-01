4a2c8baa5305ce65c87ddf6c337443c6591fba46
Mauve-negi <116134417+Mauve-negi@users.noreply.github.com>
2023-04-01 13:59:58 +0000

    [Finish]Rails6-1_bookers1

--- db/migrate/20230401122725_add_title_to_create_books.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToCreateBooks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :create_books, :title, :string
-  end
-end
--- db/migrate/20230401123408_add_address_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddAddressToBooks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :books, :title, :string
-  end
-end
